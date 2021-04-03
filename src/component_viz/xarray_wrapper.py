2719d6c2447bc4eb247cb873ea9e22a3afe763e4
Marie Roald <roald.marie@gmail.com>
2021-04-03 17:42:06 +0200

    Fixed bug with order of coordinates and added wrapping of CPTensors

--- src/component_viz/xarray_wrapper.py
+++ src/component_viz/xarray_wrapper.py
@@ -5,9 +5,12 @@ import pandas as pd
 def label_factor_matrices(factor_matrices, dataset):
     factor_matrices = [
         pd.DataFrame(factor_matrix, index=dataset.coords[dim_name])
-        for factor_matrix, dim_name in zip(factor_matrices, dataset.coords)
+        for factor_matrix, dim_name in zip(factor_matrices, dataset.dims)
     ]
-    for factor_matrix, dim_name in zip(factor_matrices, dataset.coords):
+    for factor_matrix, dim_name in zip(factor_matrices, dataset.dims):
         factor_matrix.index.name = dim_name
     return factor_matrices
 
+
+def label_cp_tensor(cp_tensor, dataset):
+    return (cp_tensor[0], label_factor_matrices(cp_tensor[1], dataset))
