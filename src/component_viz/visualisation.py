bcfdd07a3f61a33e679c43b21ecb189b4345fd8d
Marie Roald <roald.marie@gmail.com>
2021-06-11 20:11:56 +0200

    First drafts for statistical plots

--- src/component_viz/visualisation.py
+++ src/component_viz/visualisation.py
@@ -1,8 +1,69 @@
 from .model_evaluation import estimate_core_tensor
-import matplotlib.pyplot as plt
+from .outliers import compute_leverage, compute_slabwise_sse, compute_outlier_info
+from .factor_tools import construct_cp_tensor
+from .outliers import _LEVERAGE_NAME, _SLABWISE_SSE_NAME
 import numpy as np
+import matplotlib.pyplot as plt
+from scipy import stats
+import statsmodels.api as sm
+
+#TODO: visualisation or vizualisation?
+def histogram_of_residuals(cp_tensor, X, ax=None, standardised=True, **kwargs):
+    # TODO: docstring
+    estimated_X = construct_cp_tensor(cp_tensor)
+    residuals = (estimated_X - X).ravel()
+
+    if ax is None:
+        ax = plt.gca()
+    if standardised:
+        residuals = residuals/np.std(residuals)
+        ax.set_xlabel("Standardised residuals")
+    else:
+        ax.set_xlabel("Residuals")
+
+    ax.hist(residuals, **kwargs)
+    ax.set_ylabel("Frequency")
+    ax.set_title("Histogram of residuals")
+    
+    return ax
 
 
+def residual_qq(cp_tensor, X, ax=None, **kwargs):
+    #TODO: qq plot or prob plot?
+    #TODO: pingouin plot
+    estimated_X = construct_cp_tensor(cp_tensor)
+    residuals = (estimated_X - X).ravel()
+
+    if ax is None:
+        ax = plt.gca()
+
+    #res = stats.probplot(residuals, plot=ax)
+    sm.qqplot(residuals, ax=ax, **kwargs)
+
+    return ax
+
+#TODO: mode or axis?
+def outlier_plot(cp_tensor, X, mode=0, rule_of_thumbs=None, ax=None):
+    # TODO: rule of thumbs
+    weights, factor_matrices = cp_tensor
+    factor_matrix = factor_matrices[mode]
+
+    outlier_info = compute_outlier_info(cp_tensor, X, axis=mode)
+    
+    if ax is None:
+        ax = plt.gca()
+
+    ax.plot(outlier_info[f"{_LEVERAGE_NAME}"], outlier_info[f"{_SLABWISE_SSE_NAME}"], 'o')
+    ax.set_xlabel("Leverage score")
+    ax.set_ylabel("Slabwise SSE")
+    ax.set_title("Outlier plot")
+    for x, y, s in zip(outlier_info[f"{_LEVERAGE_NAME}"], outlier_info[f"{_SLABWISE_SSE_NAME}"], outlier_info.index):
+        ax.text(x,y,s)
+    return ax
+
+
+
+# TODO: Core element image plot
 def core_element_plot(cp_tensor, X, normalised=False, ax=None):
     # TODO: docstring
     weights, factors = cp_tensor
