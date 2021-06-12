72ac3814c23054b2e01d1c4232a1d921fb443aa1
Yngve Mardal Moe <yngve.m.moe@gmail.com>
2021-06-12 18:50:10 +0200

    Added tests for the outlier detection code

---
+++ tests/test_outliers.py
@@ -0,0 +1,37 @@
+import numpy as np
+from component_vis.outliers import compute_leverage, compute_slabwise_sse
+import pytest
+
+#TODO: test with xarrays as well
+def test_leverage(rng):
+    # TODO: Find more properties of leverages
+    # TODO: Manually check for small set
+    N, R = 10,3
+    A = rng.standard_normal(size=(N,R))
+    compute_leverage(A)
+    pass
+
+def test_leverages_sum_to_R(rng):
+    N, R = 10,3
+    A = rng.standard_normal(size=(N,R))
+    leverages = compute_leverage(A)
+    assert sum(leverages) == pytest.approx(R)
+
+def test_leverages_one_datapoint(rng):
+    N, R = 10,3
+    A = rng.standard_normal(size=(N,R))
+    datapoint = 3
+    A[:3, 0] = 0
+    A[3+1:,0] = 0
+
+    leverages = compute_leverage(A)
+    assert leverages[datapoint] == pytest.approx(1)
+
+def test_slabwise_sse_sum_equals_total_sse(rng):
+    tensor1 = rng.standard_normal(size=(10,20,30,40))
+    tensor2 = rng.standard_normal(size=(10,20,30,40))
+    slab_sse = compute_slabwise_sse(tensor1, tensor2, normalise=False)
+    total_sse = np.sum((tensor1 - tensor2)**2)
+    assert np.sum(slab_sse) == pytest.approx(total_sse)
+    
+
