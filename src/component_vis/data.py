2152fbfc8fe8f3dc3922b2f61b2dbe6785ebdaf8
Yngve Mardal Moe <yngve.m.moe@gmail.com>
2021-07-30 18:35:41 +0200

    Added data module that can download the aminoacids dataset

---
+++ src/component_vis/data.py
@@ -0,0 +1,42 @@
+import zipfile
+from io import BytesIO
+import requests
+from scipy.io import loadmat
+import xarray as xr
+
+
+class RemoteZip:
+    def __init__(self, url):
+        req = requests.get(url)
+        self.zip = zipfile.ZipFile(BytesIO(req.content))
+
+    @property
+    def contents(self):
+        return [f.filename for f in self.zip.infolist()]
+  
+    def extract_file(self, filename):
+        file_contents = self.zip.read(filename)
+        data = BytesIO(file_contents)
+        return data
+  
+    def extract_all(self):
+        return {filename: self.extract_file(filename) for filename in self.contents}
+
+
+def load_aminoacids():
+    print('Loading Aminoacids dataset from "PARAFAC. Tutorial and applications" by Rasmus Bro')
+    aminoacids_zip = RemoteZip("http://models.life.ku.dk/sites/default/files/Amino_Acid_fluo.zip")
+    matlab_variables = loadmat(aminoacids_zip.extract_file('amino.mat'))
+
+    I, K, J = matlab_variables['DimX'].squeeze().astype(int)
+    X = matlab_variables['X'].reshape(I, J, K)
+    emission_frequencies = matlab_variables['EmAx'].squeeze()
+    excitation_frequencies = matlab_variables['ExAx'].squeeze()
+    coords_dict = {
+        "Samples": list(range(I)), 
+        "Emission frequency": emission_frequencies,
+        "Excitation frequency": excitation_frequencies,
+    }
+    dims = ["Samples", "Excitation frequency", "Emission frequency"]
+    
+    return xr.DataArray(X, dims=dims, coords=coords_dict)
