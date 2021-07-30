68a647fa89370731dd166f3a68e2d28c77134121
Yngve Mardal Moe <yngve.m.moe@gmail.com>
2021-07-30 18:55:01 +0200

    Added todos and docstring

--- src/component_vis/_utils.py
+++ src/component_vis/_utils.py
@@ -14,9 +14,16 @@ def is_iterable(x):
 
 
 def unfold_tensor(tensor, mode):
+    # TODO: Docstring for unfold_tensor
     dataset = np.asarray(tensor)
     return np.moveaxis(dataset, mode, 0).reshape(dataset.shape[mode], -1)
 
 
 def extract_singleton(x):
+    """Extracts a singleton from an array.
+
+    This is useful whenever XArray is used, since many NumPy functions that
+    return a number will return an XArray singleton.
+    """
+    # TODO: Change code so this utility is used
     return np.asarray(x).item()
