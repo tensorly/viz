02a1ddde930eff6b4275802d76da0229164b727a
Marie Roald <roald.marie@gmail.com>
2021-11-26 10:25:35 +0100

    Added test for permuting CP tensors

--- tests/test_postprocessing.py
+++ tests/test_postprocessing.py
@@ -4,7 +4,6 @@ from component_vis import postprocessing
 from component_vis import factor_tools
 
 
-
 def test_normalise_cp_tensor_normalises(rng):
     A = rng.standard_normal((10, 3))
     B = rng.standard_normal((20, 3))
@@ -23,6 +22,7 @@ def test_normalise_cp_tensor_normalises(rng):
 
     for factor_matrix in normalised_cp_tensor[1]:
         np.testing.assert_allclose(np.linalg.norm(factor_matrix, axis=0), 1)
+
 
 def test_normalise_cp_tensor_does_not_change_tensor(rng):
     A = rng.standard_normal((10, 4))
@@ -53,6 +53,7 @@ def test_distribute_weights_in_one_mode_does_not_change_tensor(rng):
         redistributed_dense_tensor = factor_tools.construct_cp_tensor(redistributed_cp_tensor)
         np.testing.assert_allclose(dense_tensor, redistributed_dense_tensor)
 
+
 def test_distribute_weights_in_one_mode_distributes_correctly(rng):
     A = rng.standard_normal(size=(10, 4))
     B = rng.standard_normal(size=(11, 4))
@@ -70,6 +71,7 @@ def test_distribute_weights_in_one_mode_distributes_correctly(rng):
             if i != mode:
                 np.testing.assert_allclose(np.linalg.norm(new_factor_matrix, axis=0), np.ones_like(new_factor_matrix[0]))
 
+
 def test_distribute_weights_evenly_does_not_change_tensor(rng):
     A = rng.standard_normal((10, 4))
     B = rng.standard_normal((11, 4))
@@ -84,6 +86,7 @@ def test_distribute_weights_evenly_does_not_change_tensor(rng):
 
     np.testing.assert_allclose(dense_tensor, redistributed_cp_tensor)
 
+
 def test_distribute_weights_evenly(rng):
     A = rng.standard_normal((10, 4))
     B = rng.standard_normal((11, 4))
@@ -97,6 +100,7 @@ def test_distribute_weights_evenly(rng):
     np.testing.assert_allclose(np.linalg.norm(new_factors[0], axis=0), np.linalg.norm(new_factors[1], axis=0))
     np.testing.assert_allclose(np.linalg.norm(new_factors[0], axis=0), np.linalg.norm(new_factors[2], axis=0))
     np.testing.assert_allclose(new_weights, np.ones_like(new_weights))
+
 
 def test_resolve_cp_sign_indeterminacy_does_not_change_tensor(rng):
     A = rng.standard_normal((10, 4))
@@ -144,7 +148,33 @@ def test_resolve_cp_sign_indeterminacy_flips_negative_components_for_nonnegative
             assert np.all(wrong_flip_cp_tensor[1][flip2] <= 0), "Did not setup test correctly"
 
 
+def test_permute_cp_tensor(rng):
+    # Create a rank-3 CP tensor and a copy of it that is permuted. Test if postprocessing.permute_cp_tensor permutes it back
+    # Modify the permuted copy so that one of its components consists of random vectors, check again that the permutation is correct
+    # Create a copy of the rank-3 CP tensor, permute it and remove a component. Align the 3-component to the 2-component model and check that the first two components are the two components present in the two-component model
+    A = rng.standard_normal((10, 4))
+    B = rng.standard_normal((11, 4))
+    C = rng.standard_normal((12, 4))
+    w = rng.uniform(size=(4,))
 
+    cp_tensor = (w, (A, B, C))
+    
+    permutation = [2, 1, 3, 0]
+    cp_tensor_permuted = (w[permutation], (A[:, permutation], B[:, permutation], C[:, permutation]))
+    cp_tensor_permuted_back = postprocessing.permute_cp_tensor(cp_tensor_permuted, cp_tensor)
+    assert factor_tools.check_cp_tensors_equals(cp_tensor_permuted_back, cp_tensor)
     
 
+    # Check permutation comparing against fewer components
+    permutation_2comp = [1, 3]
+    cp_tensor_permuted2 = (w[permutation_2comp], (A[:, permutation_2comp], B[:, permutation_2comp], C[:, permutation_2comp]))
+    aligned_cp_tensor = postprocessing.permute_cp_tensor(cp_tensor, cp_tensor_permuted2)
 
+    aligned_weights, aligned_factors = aligned_cp_tensor
+   
+    assert np.all(aligned_weights[:2] == cp_tensor_permuted2[0])
+    for factor1, factor2 in zip(cp_tensor_permuted2[1], aligned_factors):
+        np.testing.assert_allclose(factor1, factor2[:, :2])
+    
+    # Check that the permutation is equivalent to the unpermuted decomposition
+    assert factor_tools.check_cp_tensors_equivalent(cp_tensor, aligned_cp_tensor)
