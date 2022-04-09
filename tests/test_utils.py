7bf7b16bc13cf7f668c3673607cdbc3896d0e02b
Yngve Mardal Moe <yngve.m.moe@gmail.com>
2022-04-09 07:26:01 +0200

    Added test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -29,3 +29,30 @@ def test_is_iterable_is_true_for_iterables(iterable):
 def test_is_iterable_is_false_for_noniterables(non_iterable):
     assert not utils.is_iterable(non_iterable)
 
+
+def test_extract_singleton(rng):
+    scalar = rng.normal()
+
+    singleton_list = [scalar]
+    assert utils.extract_singleton(singleton_list) == scalar
+
+    singleton_array_1d = np.array([scalar])
+    assert utils.extract_singleton(singleton_array_1d) == scalar
+
+    singleton_array_2d = np.array([[scalar]])
+    assert utils.extract_singleton(singleton_array_2d) == scalar
+
+    singleton_dataframe = pd.DataFrame([scalar])
+    assert utils.extract_singleton(singleton_dataframe) == scalar
+
+    singleton_series = pd.Series([scalar])
+    assert utils.extract_singleton(singleton_series) == scalar
+
+    singleton_xarray_1d = xr.DataArray([scalar])
+    assert utils.extract_singleton(singleton_xarray_1d) == scalar
+
+    singleton_xarray_2d = xr.DataArray([[scalar]])
+    assert utils.extract_singleton(singleton_xarray_2d) == scalar
+
+    singleton_xarray_3d = xr.DataArray([[[scalar]]])
+    assert utils.extract_singleton(singleton_xarray_3d) == scalar
