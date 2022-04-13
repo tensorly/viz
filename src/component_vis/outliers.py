Made tests and fixed bugs due to finite precision

src/component_vis/outliers.py
@@ -349,8 +349,8 @@ def get_leverage_outlier_threshold(leverage_scores, method="p_value", p_value=0.
     >>> print(f"95% confidence interval for the false positive rate: [{fpr_low:.4f}, {fpr_high:.4f}]")
     95% confidence interval for the false positive rate: [0.0494, 0.0515]
     """
-    num_samples = len(leverage_scores)
-    num_components = np.sum(leverage_scores)
+    num_samples = int(round(len(leverage_scores)))
+    num_components = int(round(np.sum(leverage_scores)))
 
     method = method.lower()
     if method == "huber lower":
@@ -369,14 +369,18 @@ def get_leverage_outlier_threshold(leverage_scores, method="p_value", p_value=0.
         if n <= p:
             raise ValueError("Cannot use P-value when there are fewer samples than components.")
 
+        # TODO: Try with n - p - 1, and try maths too compare with hotelling
         F = stats.f.isf(p_value, p - 1, n - p)
         F_scale = F * (p - 1) / (n - p)
         # Solve the equation (h + (1/n)) / (1 - h) = F_scale:
         return (F_scale + (1 / n)) / (1 + F_scale)
     elif method == "hotelling":
         I, R = num_samples, num_components
+        if I <= R + 1:
+            raise ValueError("Cannot use Hotelling P-value when there are fewer samples than components minus one.")
+
         F = stats.f.isf(p_value, R, I - R - 1)
-        B = (R / (I - R - 1)) * F / (1 + (R / (I - R - 1)) * F)
+        B = F * (R / (I - R - 1)) / (1 + (R / (I - R - 1)) * F)
         # Remove the square compared to Nomikos & MacGregor since the leverage is:
         #  A(AtA)^-1 At,
         # not
@@ -389,7 +393,7 @@ def get_leverage_outlier_threshold(leverage_scores, method="p_value", p_value=0.
         )
 
 
-def get_slab_sse_outlier_threshold(slab_sse, method="p_value", p_value=0.05, ddof=1):
+def get_slab_sse_outlier_threshold(slab_sse, method="p-value", p_value=0.05, ddof=1):
     r"""Compute rule-of-thumb threshold values for suspicious residuals.
 
     One way to determine possible outliers is to examine how well the model describes
@@ -412,7 +416,7 @@ def get_slab_sse_outlier_threshold(slab_sse, method="p_value", p_value=0.05, ddo
     Parameters
     ----------
     slab_sse : np.ndarray or pd.DataFrame
-    method : {"two_sigma", "p_value"}
+    method : {"two_sigma", "p-value"}
     p_value : float (optional, default=0.05)
         If ``method="p-value"``, then this is the p-value used for the cut-off.
 

tests/test_outliers.py
@@ -1,7 +1,14 @@
 import numpy as np
 import pytest
 
-from component_vis.outliers import compute_leverage, compute_slabwise_sse
+from component_vis import factor_tools
+from component_vis.data import simulated_random_cp_tensor
+from component_vis.outliers import (
+    compute_leverage,
+    compute_slabwise_sse,
+    get_leverage_outlier_threshold,
+    get_slab_sse_outlier_threshold,
+)
 
 
 # TODO: test with xarrays as well. should we have some parameterization of fixture or something to test xarrays as well
@@ -43,3 +50,66 @@ def test_slabwise_sse_sum_equals_total_sse(rng):
     slab_sse = compute_slabwise_sse(tensor1, tensor2, normalise=False)
     total_sse = np.sum((tensor1 - tensor2) ** 2)
     assert np.sum(slab_sse) == pytest.approx(total_sse)
+
+
+def test_slab_leverage_threshold_invalid_method(rng):
+    # Test that value error is raised when method is not among:
+    # 'huber lower', 'huber higher', 'hw lower', 'hw higher', 'p-value', and 'hotelling'
+
+    N, R = 10, 3
+    A = rng.standard_normal(size=(N, R))
+    leverage = compute_leverage(A)
+    with pytest.raises(ValueError):
+        get_leverage_outlier_threshold(leverage, method="invalid method")
+
+
+def test_slab_leverage_threshold_p_value_one_component(rng):
+    # Test that value error is raised when rank is 1 and method is p_value
+    N, R = 10, 1
+    A = rng.standard_normal(size=(N, R))
+    leverage = compute_leverage(A)
+    with pytest.raises(ValueError):
+        get_leverage_outlier_threshold(leverage, method="p-value")
+
+
+def test_slab_leverage_threshold_p_value_too_large_rank(rng):
+    # Test that value error is raised when I=10, rank is 10 and method is p_value
+    N, R = 10, 10
+    A = rng.standard_normal(size=(N, R))
+    leverage = compute_leverage(A)
+    with pytest.raises(ValueError):
+        get_leverage_outlier_threshold(leverage, method="p-value")
+
+
+def test_slab_leverage_threshold_hotelling_too_large_rank(rng):
+    # Create random 10 component CP tensor with I=11
+    # Compute leverages in first mode
+    # Test that value error is raised when method is p_value
+    N, R = 11, 10
+    A = rng.standard_normal(size=(N, R))
+    leverage = compute_leverage(A)
+    with pytest.raises(ValueError):
+        get_leverage_outlier_threshold(leverage, method="hotelling")
+
+
+@pytest.mark.parametrize("labelled", [True, False])
+def test_slab_sse_threshold_invalid_method(seed, labelled):
+    # Test that value error is raised when method is not 'two sigma' or 'p-value'
+    X1 = simulated_random_cp_tensor((10, 11, 12), 4, seed=seed, labelled=labelled)[1]
+    X2 = simulated_random_cp_tensor((10, 11, 12), 4, seed=seed, labelled=labelled)[1]
+    sse = compute_slabwise_sse(X1, X2)
+    with pytest.raises(ValueError):
+        get_slab_sse_outlier_threshold(sse, method="invalid method")
+
+
+@pytest.mark.parametrize("labelled", [True, False])
+def test_check_cp_tensors_equivalent_same_weights_different_components(seed, labelled):
+
+    cp_tensor1 = simulated_random_cp_tensor((10, 11, 12), 4, seed=seed, labelled=labelled)[0]
+    cp_tensor2 = simulated_random_cp_tensor((10, 11, 12), 4, seed=seed + 1, labelled=labelled)[0]
+    cp_tensor2 = factor_tools.permute_cp_tensor(cp_tensor2, reference_cp_tensor=cp_tensor1)
+    cp_tensor1 = factor_tools.normalise_cp_tensor(cp_tensor1)
+    cp_tensor2 = factor_tools.normalise_cp_tensor(cp_tensor2)
+
+    cp_tensor3 = cp_tensor1[0], cp_tensor2[1]  # same weights as cp_tensor1, but different components
+    assert not factor_tools.check_cp_tensors_equivalent(cp_tensor1, cp_tensor3)